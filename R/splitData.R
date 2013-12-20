b79a040fea198b1d969fe909449ba270b2590d53
Joseph Guillaume <josephguillaume@gmail.com>
2013-12-20 16:07:03 +1100

    crossValidate and splitData: Added named periods and names for model structure and fitBy

--- R/splitData.R
+++ R/splitData.R
@@ -3,12 +3,16 @@
 ## Copyright (c) Joseph Guillaume <josephguillaume@gmail.com>
 ##
 
-splitData<-function(object,start.dates,end.dates){
+splitData<-function(object,start.dates=NULL,end.dates=NULL,periods=NULL){
   ## Split data according to start.dates and end.dates
+  if(!is.null(periods)){
+    start.dates <- sapply(periods,head,1)
+    end.dates <- sapply(periods,tail,1)
+  }
   cv.data <- mapply(window,start=start.dates,end=end.dates,MoreArgs=list(x=observed(object,all=TRUE,select=TRUE)),SIMPLIFY=FALSE)
   rl=as.runlist(mapply(update,newdata=cv.data,MoreArgs=list(object=object),SIMPLIFY=FALSE))
-  cv.names=paste(start.dates,end.dates,sep="_")
-  names(rl)<-cv.names
+  if(!is.null(names(periods))) names(rl)<-names(periods)
+  else names(rl) <- paste(start.dates,end.dates,sep="_")
   return(rl)
 }
 
